1663d84d3ce44d60d3ab6840b20de6508de0997f
sergey <szaprudnov@hotmail.com>
2023-07-01 23:15:03 +0300

    импорт библиотеки добавление маршрута

--- my_home/urls.py
+++ my_home/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from my_home.views import index
+
+app_name = 'my_home'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='index')
 ]
