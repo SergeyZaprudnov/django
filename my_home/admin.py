0e0747215379986c3d27b45b82697c67faa7ea93
sergey <szaprudnov@hotmail.com>
2023-07-03 14:27:59 +0300

    creation

--- my_home/admin.py
+++ my_home/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from my_home.models import Product, Category
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name', 'purchase_price', 'category', 'image']
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name']
