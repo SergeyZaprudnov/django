f58167a5783ad05808f49d9f984140ddee7fb7f9
sergey <szaprudnov@hotmail.com>
2023-07-08 00:09:01 +0300

    add

--- my_blog/models.py
+++ my_blog/models.py
@@ -6,12 +6,13 @@ NULLABLE = {'blank': True, 'null': True}
 
 class Blog(models.Model):
     name = models.CharField(max_length=100, verbose_name="Название")
-    slug = models.CharField(max_length=255)
+    slug = models.CharField(max_length=150, verbose_name="slug")
     description = models.TextField(null=True, blank=True, verbose_name="Описание")
     image = models.ImageField(upload_to='my_home/products/', **NULLABLE)
     created_at = models.DateTimeField(default=timezone.now)
-    is_published = models.BooleanField(default=True)
-    blog_views = models.IntegerField(verbose_name="blog_views", default=0)
+
+    is_published = models.BooleanField(default=True, verbose_name="опубликовано")
+    blog_views = models.IntegerField(verbose_name="просмотры", default=0)
 
     def __str__(self):
         return f'{self.name}'
--- my_blog/views.py
+++ my_blog/views.py
@@ -1,6 +1,8 @@
-from django.urls import reverse_lazy
-from django.utils.text import slugify
+from django.urls import reverse_lazy, reverse
+
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+from pytils.translit import slugify
+
 from my_blog.models import Blog
 
 
@@ -10,22 +12,48 @@ class BlogCreateView(CreateView):
     success_url = reverse_lazy('my_blog:list')
 
     def form_valid(self, form):
-        form.instance.slug = slugify(form.instance.name)
+        if form.is_valid():
+            new_blog = form.save()
+            new_blog.slug = slugify(new_blog.name)
+            new_blog.save()
+
         return super().form_valid(form)
 
 
 class BlogUpdateView(UpdateView):
     model = Blog
     fields = ('name', 'description',)
-    success_url = reverse_lazy('my_blog:list')
+   # success_url = reverse_lazy('my_blog:list')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_blog = form.save()
+            new_blog.slug = slugify(new_blog.name)
+            new_blog.save()
+
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('my_blog:view', args=[self.kwargs.get('pk')])
 
 
 class BlogListView(ListView):
     model = Blog
 
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(is_published=True)
+        return queryset
+
 
 class BlogLDetailView(DetailView):
     model = Blog
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.blog_views += 1
+        self.object.save()
+        return self.object
 
 
 class BlogDeleteView(DeleteView):
