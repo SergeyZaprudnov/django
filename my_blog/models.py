7ffc22d8f53e04745d8f04676e0efa846f0f8cc4
sergey <szaprudnov@hotmail.com>
2023-07-07 11:55:34 +0300

    add

---
+++ my_blog/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.utils import timezone
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Blog(models.Model):
+    name = models.CharField(max_length=100, verbose_name="blog_name")
+    slug = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True, verbose_name="blog_description")
+    image = models.ImageField(upload_to='products/', **NULLABLE)
+    created_at = models.DateTimeField(default=timezone.now)
+    is_published = models.BooleanField(default=True)
+    blog_views = models.IntegerField(verbose_name="blog_views", default=0)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = "Blog"
+        verbose_name_plural = "Blogs"
